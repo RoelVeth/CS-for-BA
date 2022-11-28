d72896c8d8f8ffd5c863dadb60e50e14a354d7dd
RoelVeth <roel.veth@gmail.com>
2022-11-28 14:35:51 +0100

    Started code for opening json

--- CS assignment.py
+++ CS assignment.py
@@ -4,4 +4,45 @@ Created on Mon Nov 28 13:11:48 2022
 
 @author: Roel
 """
+# Imports
+import numpy as np
+import json
+
+# Loading the data
+file = open('TVs-all-merged.json')
+data = json.load(file)
+
+for i in range(5):#range(len(data)):
+    print(data[i].key)
+    # for j in range(len(data[i])):
+    #     print(data[i].key)
+
+# my_list = []
+# for i in data.values():
+#     my_list.append(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
