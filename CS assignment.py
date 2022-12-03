f08cafd66b8a540635d80e3d6189e4c046c577f4
RoelVeth <roel.veth@gmail.com>
2022-12-03 12:27:35 +0100

    Data loading has been finished, but only titles

--- CS assignment.py
+++ CS assignment.py
@@ -7,19 +7,37 @@ Created on Mon Nov 28 13:11:48 2022
 # Imports
 import numpy as np
 import json
+import re
+from sklean.metrics import jaccard_score
 
-# Loading the data
+
+# Loading the data file
 file = open('TVs-all-merged.json')
 data = json.load(file)
 
-for i in range(5):#range(len(data)):
-    print(data[i].key)
-    # for j in range(len(data[i])):
-    #     print(data[i].key)
 
-# my_list = []
-# for i in data.values():
-#     my_list.append(i)
+# Load the data to a list TODO: Check if this can be done in a dictionary, this could be faster?
+tv = [] # List to fill
+for i in data.keys():
+    for j in range(len(data[i])):
+        tv.append(data[i][j]['title'])
+
+
+# Data cleaning
+# Code carlos
+wordsWithStrignAndChar = set()
+for i in tv:
+    tmp = re.findall('[a-zA-Z0-9]*(([0-9]+[^0-9^,^ ]+)|([^0-9^,^ ]+[0-9]+))[a-zA-Z0-9]*',i)
+
+
+
+# Initialize LSH
+
+
+
+# Candidate pair selection by LSH
+
+
 
 
 
