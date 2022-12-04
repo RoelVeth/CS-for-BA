77cd7545641a1890f92409067194c939e59ddc48
RoelVeth <roel.veth@gmail.com>
2022-12-04 21:03:24 +0100

    Added signature matrix and LSH

--- CS assignment.py
+++ CS assignment.py
@@ -11,6 +11,35 @@ import re
 from sklearn.metrics import jaccard_score
 
 
+### Parameters
+n = 100 # Number of minhashes/rows in signature matrix
+# t = # Threshold
+LSH_number_of_bands = 20# Number of bands in LSH
+LSH_number_of_rows = 5# Number of rows in each band in LSH
+
+ 
+### Function
+# Found on 4-12-2022 at: https://datagy.io/python-prime-numbers/
+def is_prime(number):
+    if number > 1:
+        for num in range(2, int(number**0.5) + 1):
+            if number % num == 0:
+                return False
+        return True
+    return False
+
+def find_next_prime(number):
+    while True:
+        if is_prime(number):
+            return number
+        elif (number%2 == 0): # Check if p is an even number, if yes, make uneven
+            number += 1
+        else:
+            number += 2
+
+
+
+
 ### Loading the data file
 file = open('TVs-all-merged.json')
 data = json.load(file)
@@ -25,35 +54,25 @@ for i in data.keys():
 
 ### Data cleaning
 tv_titles = tv_uncleaned # Keep uncleaned stored for comparison
+
 # Replace all capital letters with non-capital
 tv_titles = [i.lower() for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
-tv_titles = [i.replace("(", "") for i in tv_titles]
 
-
-
-
+# Remove symbols make tv titles more unclear
+tv_titles = [i.replace("(", "") for i in tv_titles]
+tv_titles = [i.replace(")", "") for i in tv_titles]
+tv_titles = [i.replace("[", "") for i in tv_titles]
+tv_titles = [i.replace("]", "") for i in tv_titles]
+tv_titles = [i.replace("{", "") for i in tv_titles]
+tv_titles = [i.replace("}", "") for i in tv_titles]
+tv_titles = [i.replace(":", "") for i in tv_titles]
+tv_titles = [i.replace("/", "") for i in tv_titles]
 
 # Replace variants of inch with 'inch'
 tv_titles = [i.replace("\"", "inch") for i in tv_titles]
 tv_titles = [i.replace("\' \'", "inch") for i in tv_titles]
 tv_titles = [i.replace("''", "inch") for i in tv_titles]
+tv_titles = [i.replace("'", "inch") for i in tv_titles]
 tv_titles = [i.replace(" in", "inch") for i in tv_titles]
 tv_titles = [i.replace("inches", "inch") for i in tv_titles]
 tv_titles = [i.replace("-inch", "inch") for i in tv_titles]
@@ -68,18 +87,8 @@ tv_titles = [i.replace(" hertz", "hz") for i in tv_titles]
 
 
 
-### Obtaining binary vectors
-# Creating the model word set
+### Creating the model word set
 mw_set = set()
-# Old
-# for i in tv_titles:
-#     modelwords = re.findall(r'[a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*', i, flags = re.IGNORECASE) # TODO: it seems to miss 720p, what can I do about that?
-#     modelword = modelwords.groups().replace(" ", "")  # TODO: Rick had dit erachter, moet dat blijven?.replace(" ", "")
-#     mw_set.add(modelword)
-# mw_list = list(mw_set) # Change set to list to allow for indices
-
-
-#new
 for i in tv_titles:
     modelwords = re.finditer('[a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*', i)
     for k in modelwords:
@@ -87,22 +96,8 @@ for i in tv_titles:
 mw_list = list(mw_set) # Change set to list to allow for indices
 
 
-# For testing regex
-# modelline = tv_titles[0].split(" ")
-# modelwords = re.finditer('[a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*', tv_titles[0]) # TODO: it seems to miss 720p, what can I do about that?
-# # modelword0 = modelwords.group(0)
-# # modelword1 = modelwords.group(1)
-# testset = set()
-# for x in modelwords:
-#     testset.add(x.group())
-# mw_set.add(modelwords)
-# mw_list = list(mw_set) # Change set to list to allow for indices
-
-
-
-# Create signature matrix 
-# TODO: Is dit de signature matrix of is het iets anders? 
-B = np.zeros((len(mw_list),len(tv_titles)))
+### Create the characteristic matrix
+B = np.zeros((len(mw_list),len(tv_titles))) # Empty characteristic matrix
 for tv in tv_titles:
     for mw in mw_list:
         if mw in tv:
@@ -110,34 +105,57 @@ for tv in tv_titles:
 
 
 
+### Creating the signature matrix
+S = np.inf * np.ones((n,len(tv_titles))) # Initialize signature matrix with each element +inf
+
+# Parameters for the hash functions
+a = np.random.randint(0, 10*len(mw_set), size = n) # Choose len(mw_set) as upper bound to find good numbers
+b = np.random.randint(1, 10*len(mw_set), size = n)
+p = find_next_prime(len(mw_set)) # The mod value should be greater than the number of elements
+
+# Filling the signature matrix
+# TODO: kijken of dit wel écht goed gaat, nu erg veel columns die alleen maar inf zijn (hogere n waarde werkt beter)
+h = np.zeros((n,1)) # Initiate the hash values array
+for r in range(len(B)): # For each row r
+    for n in range(n):
+        h[n] = (a[n] + b[n] * n) % p # TODO: Als je hier de indices veranderrd naar iets anders dan 'n' dan lijkt het stuk te gaan
+    for c in range(len(B[0])): # For each column c
+        if B[r,c] == 1:
+            for i in range(n):
+                if h[i] < S[i,c]:
+                    S[i,c] = h[i]
 
 
-# Uitleg van die grote RegEx string
-# [a-zA-Z0-9]* #zero or more alphanumericals
-# (
-#  ([0-9]+[^0-9^,^ ]+ # Ten minste 1 numerica gevolgd met ten minste 1 [niet nummer, niet comma, niet spatie] 
-#   )| # Óf
-#   ([^0-9^,^ ]+[0-9]+) # ten minste 1 [niet nummer, niet comma, niet spatie], gevolgd door ten minste 1 numerical
-# )
-# [a-zA-Z0-9]* #zero or more alphanumericals
+### LSH
+# Parameters
+nob = LSH_number_of_bands # number of bands
+rpb = LSH_number_of_rows # rows per band
+if nob*rpb != len(S):
+    print("Number of bands times number of rows is not equal to length of Signature matrix! \nChoose different values so that b*r=n! ")
 
 
+# Divide signature matrix in bands
+LSH_matrix = np.zeros((S.shape[0]//nob,S.shape[1],nob))
+for b in range(nob):
+    for i in range(rpb):
+        LSH_matrix[i,:,b] = S[b*rpb+i,:]
 
 
-
-# Building signature matrix
-
-# Initialize LSH
-
-
-
+# Hash into buckets
+candidate_pairs = set()
+buckets_number = find_next_prime(2*len(tv_titles)) # Ensure that nr buckets > nr titles
+bucket_values = np.zeros((buckets_number,len(tv_titles)))
+alpha = np.random.randint(0, 10*len(mw_set)) # Choose len(mw_set) as upper bound to find good numbers
+beta = np.random.randint(1, 10*len(mw_set))
+for b in range(nob):
+    for i in range(len(S[0])):
+        # Hash function on LSH_matrix[:,i,b]
+        x = LSH_matrix[:,i,b].sum(axis=0)
+        if x != np.inf:
+            y = (alpha + beta*int(x)) % p
+            bucket_values[y,i] += 1
+        
 # Candidate pair selection by LSH
-
-
-
-
-
-
 
 
 
